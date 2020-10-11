2a281e55dcf980dcd6045c8dc1c67cd613f35630
UmedJadhav <umed.jadhav@somaiya.edu>
2020-10-12 00:51:26 +0530

    feat: Created the Login to Spotify Component

--- spotify_overview_client/src/App.js
+++ spotify_overview_client/src/App.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
+import LoginPage from './components/Login/Login.component';
 
-function App() {
+import styled from 'styled-components/macro';
+import GlobalStyles from './styles/global_styles';
+
+const AppContainer = styled.div`
+  height: 100%;
+  min-height: 100vh;
+`;
+
+function App () {
   return (
-    <div className="App">
-      Welcome
-    </div>
+    <AppContainer>
+      <GlobalStyles/>
+      <LoginPage/>
+    </AppContainer>
   );
-}
+} 
 
 export default App;
---
+++ spotify_overview_client/src/components/Login/Login.component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import  theme  from '../../styles/theme';
+import mixins from '../../styles/mixins';
+import Main from '../../styles/Main';
+
+const { colors, fontSizes } = theme;
+
+// TODO: Add the production link
+const LOGIN_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8888/login' : ''
+
+const Login = styled(Main)`
+    ${mixins.flexCenter};
+    flex-direction: column;
+    min-heightL 100vh;
+    h1{
+        font-size: ${fontSizes.xxl};
+    }`;
+
+
+const LoginButton = styled.a`
+    display: inline-block;
+    background-color: ${colors.green};
+    color: ${colors.white};
+    border-radius: 30px;
+    padding: 17px 35px;
+    margin: 20px 0 70px;
+    min-width: 160px;
+    font-weight: 700;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+    text-align: center;
+    &:hover, &:focus {
+        background-color: ${colors.offGreen};
+    }
+`;
+
+const LoginPage = () => (
+    <Login>
+        <h1>Spotify Overview</h1>
+        <LoginButton href={LOGIN_URL} >Log In to Spotify</LoginButton>
+    </Login>
+);
+
+export default LoginPage;
